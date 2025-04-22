c099d748246531b9d80adfe05122028c5c34ae0d
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2025-04-22 18:47:52 +0530

    app router - till navigation between pages

--- dashboard/starter-example/app/layout.tsx
+++ dashboard/starter-example/app/layout.tsx
@@ -1,11 +1,18 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  console.log('RootLayout rendered')
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <h1>Hello from me</h1>
+        {children}
+      </body>
     </html>
   );
 }
